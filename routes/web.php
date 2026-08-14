cec897dfb57ffac4e18afcf3b27620a070f53dc5
Guilherme Mendes <guilhermemendes.info@gmail.com>
2026-08-14 10:38:37 -0300

    validando parametros

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/users/{id}', function($id) {
     return 'hello word' . $id;
-});
+})->where('id', '[0-9]+');
+
 Route::get('/', function(){
     return View('welcome');
 });
