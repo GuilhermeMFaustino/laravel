864adf2ffc7587ce470c607e4023996bfce3f59c
Guilherme Mendes <guilhermemendes.info@gmail.com>
2026-08-14 10:05:54 -0300

    rota com parametro

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-/*Route::get('users', function() {
-    return 'hello word';
-});*/
-
-// Route::redirect('rota-a', 'rota-b', 301);
-
-Route::view('/welcome', 'welcome');
-
+Route::get('/users/{id}', function($id) {
+    return 'hello word' . $id;
+});
 Route::get('/', function(){
     return View('welcome');
 });
-
-
-
-Route::get('rota-b', function(){
-    return 'Rota B';
-});
-Route::match(['get', 'post'], 'users', function() {
-    return 'hello word';
-})->name('users');
