e60fca45d1609172eef4eea41e57d2a188fd7456
Guilherme Mendes <guilhermemendes.info@gmail.com>
2026-08-19 12:47:28 -0300

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,9 @@
 <?php
 
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-
-    return View('welcome');
-});
-
-/*Route::middleware(['userAgent', 'CheckToken'])->group(function () {
-    Route::get('users', function () {
-
-        dd('users');
-    });
-
-    Route::get('posts', function () {
-
-        dd('posts');
-    });
-});*/
-
-Route::get('admin', function(){
-    dd('admin');
-})->middleware('CheckToken');
-
+Route::get('/users', [UserController::class, 'index']);
 
 
 
