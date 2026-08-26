231c4196730eacc62a1e46b2f0c256fdc9d1be3b
Guilherme Mendes <guilhermemendes.info@gmail.com>
2026-08-26 16:20:05 -0300

    view e controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,8 +9,8 @@ Route::get('/', function(){
  return view('welcome');
 });
 
+Route::get('/users', [UserController::class, 'index']);
 
-
-Route::get('/profile', function(){
+/*Route::get('/profile', function(){
  return view('user.profile');
-});
+});*/
