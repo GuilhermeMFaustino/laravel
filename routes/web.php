2861ee0ce3c93a0cc78c7f7f613f452e02ebe82b
Guilherme Mendes <guilhermemendes.info@gmail.com>
2026-08-26 16:06:02 -0300

    criando primeira view

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('user/{user}', function(Request $request){
-
-    /*if($request->has(['token', 'curso'])){
-    dd('token e curso existem');
-    }*/
-
-
-    $request->whenHas('token', function($input){
-        dd('faca alguma coisa', $input);
-    });
+Route::get('/', function(){
+ return view('welcome');
 });
 
-/*Route::get('user', function(Request $request) {
 
-    // dd($request);
 
-    //dd($request->path());
-
-    //dd($request->url());
-    dd($request->fullUrl());
-    // dd($request->fullUrlWithQuery());
-    // dd($request->fullUrlIs());
-    // dd($request->is('users'));
-    // dd($request->routeIs('users'));
-    // dd($request->method());
-    // dd($request->isMethod('get'));
-
-});*/
+Route::get('/profile', function(){
+ return view('user.profile');
+});
