be92cc0a1f8c3e079a6c551f0da5e271e47b8c91
Guilherme Mendes <guilhermemendes.info@gmail.com>
2026-08-31 12:34:09 -0300

    criando stack

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App</title>
 </head>
+@stack('css')
+
 <body>
     @section('content')
      conteudo padrao
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.default')
 
 
+@push('css')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+@endpush
+
 @section('content')
     @each('user.heading', $user, 'user')
 @endsection
